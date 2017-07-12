ba73cb97e7700309b673479e7ee89e8829ee090a
NizamiKhan <sauron270695@yandex.ru>
2017-07-12 12:38:02 +0300

    учебный

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Главная</title>
+        <title>Тест проект3</title>
         <link href="/template/css/bootstrap.min.css" rel="stylesheet">
         <link href="/template/css/font-awesome.min.css" rel="stylesheet">
         <link href="/template/css/prettyPhoto.css" rel="stylesheet">
@@ -33,7 +33,7 @@
                             <div class="contactinfo">
                                 <ul class="nav nav-pills">
                                     <li><a href="#"><i class="fa fa-phone"></i> +95011331313</a></li>
-                                    <li><a href="#"><i class="fa fa-envelope"></i> bratishka@gmail.com</a></li>
+                                    <li><a href="#"><i class="fa fa-envelope"></i> phpphp@gmail.com</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -61,7 +61,7 @@
                             <div class="shop-menu pull-right">
                                 <ul class="nav navbar-nav">                                    
                                     <li><a href="#"><i class="fa fa-shopping-cart"></i> Корзина</a></li>
-                                    <li><a href="#"><i class="fa fa-user"></i> Аккаунт</a></li>
+                                    <li><a href="/user/register"><i class="fa fa-user"></i> Аккаунт</a></li>
                                     <li><a href="#"><i class="fa fa-lock"></i> Вход</a></li>
                                 </ul>
                             </div>
@@ -87,13 +87,13 @@
                                     <li><a href="/">Главная</a></li>
                                     <li class="dropdown"><a href="#">Магазин<i class="fa fa-angle-down"></i></a>
                                         <ul role="menu" class="sub-menu">
-                                            <li><a href="/catalog/">Каталог товаров</a></li>
-                                            <li><a href="/cart/">Корзина</a></li> 
+                                            <li><a href="#">Каталог товаров</a></li>
+                                            <li><a href="#">Корзина</a></li>
                                         </ul>
                                     </li> 
-                                    <li><a href="/blog/">Блог</a></li> 
-                                    <li><a href="/about/">О магазине</a></li>
-                                    <li><a href="/contacts/">Контакты</a></li>
+                                    <li><a href="#">Блог</a></li>
+                                    <li><a href="#">О магазине</a></li>
+                                    <li><a href="#">Контакты</a></li>
                                 </ul>
                             </div>
                         </div>
